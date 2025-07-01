b1f9561c2b2ba63362c04de61fdcad339a98acf6
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-01 19:29:58 -0300

    Explored statements, expressions and return functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,15 +1,31 @@
-fn function_above(thing: &str, value: i32) {
+fn function_before(thing: &str, value: i32) {
     println!("Above, of {thing}. Value is {value}");
-    another_function(33);
+    another_func_after(33);
+}
+
+fn func_implicit(value: &str) -> i32 {
+    value.parse::<i32>().expect("Impresive, LOL")
+}
+
+fn func_explicit(name: &str, surname: &str) -> String {
+    let fullname = format!("{name} {surname}").to_owned();
+    return fullname
 }
 
 fn main() {
-    another_function(23);
+    another_func_after(23);
     let thing: &str = "house";
-    let value: i32 = "22".parse().expect("LOL");
-    function_above(thing, value);
+    let value: i32 = func_implicit("22");
+    function_before(thing, value);
+    println!("{}", func_explicit("Tom", "Voznik"));
+
+    let y = {
+        let h = 6;
+        h + 1
+    };
+    println!("{y}")
 }
 
-fn another_function(x: i32) {
+fn another_func_after(x: i32) {
     println!("The value of x is: {x}");
 }
