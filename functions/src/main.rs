63dd5d91ed158022ced91b8748c6f2da91a0fd38
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-01 18:03:35 -0300

    Arguments tested in functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,12 +1,13 @@
-fn function_above() {
-    println!("Above^");
+fn function_above(thing: String, value: i32) {
+    println!("Above, of {thing}. Value is {value}");
+    another_function(33);
 }
 
 fn main() {
-    another_function();
+    another_function(23);
     function_above();
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
 }
