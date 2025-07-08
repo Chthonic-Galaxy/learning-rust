f3686b36689e891481c7e248db395e2375fec5ef
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-08 19:56:46 -0300

    Learned 'if' expressions

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let number = 9;
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    if number % 4 == 0 {
-        println!("Number is devisible by 4")
-    } else if number % 3 == 0 {
-        println!("Number is devisible by 3")
-    } else {
-        println!("Number is not divisible by either 4 or 3")
-    }
+    println!("Value of number: {number}");
+
+    let number2 = if condition { 5 } else { "six" };
+
+    println!("Value of number: {number}")
 }
