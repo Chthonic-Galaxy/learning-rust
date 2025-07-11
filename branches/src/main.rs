7ce6de1fd4d847c44ba7de763f73a304eb96d414
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-11 12:56:09 -0300

    Saw rev method

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-
-    for element in a {
-        println!("The value is: {element}");
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+    println!("LIFTOFF!!!")
 }
