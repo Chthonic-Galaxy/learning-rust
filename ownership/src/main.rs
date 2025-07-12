5494b4337d363e729d986408c0f37577bc903b1a
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-12 12:19:51 -0300

    Exploring Ownership in Rust - References and Dereferences - 6p

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
-    let m1 = String::from("Hello");
-    let m2 = String::from("World");
-    greet(m1, m2);
-    let s = format!("{} {}", m1, m2);
-}
+    let x: Box<i32> = Box::new(-1);
+    let x_abs1 = i32::abs(*x);
+    let x_abs2 = x.abs();
+    assert_eq!(x_abs1, x_abs2);
 
-fn greet(g1: String, g2: String) {
-    println!("{} {}!", g1, g2);
+    let r: &Box<i32> = &x;
+    let r_abs1 = i32::abs(**r);
+    let r_abs2 = r.abs();
+    assert_eq!(r_abs1, r_abs2);
+
+    let s = String::from("Hello");
+    let s_len1 = str::len(&s);
+    let s_let2 = s.len();
+    assert_eq!(s_len1, s_let2);
 }
