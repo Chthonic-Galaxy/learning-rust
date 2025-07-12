2e7e25c8887ea0d08023528fc53d1eb51446cf1e
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2025-07-12 10:07:32 -0300

    Exploring Ownership in Rust - 2p

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,10 @@
-fn read(y: bool) {
-    if y {
-        println!("Is true")
-    }
+fn main() {
+    let n = 5;
+    let y = minus_one(n);
+
+    println!("The value of y={y}, and of n={n}");
 }
 
-fn main() {
-    let x = true;
-    read(x);
+fn minus_one(x: i32) -> i32{
+    x - 1
 }
